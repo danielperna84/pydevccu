3831c6193961c862825f72b4135360321c48f193
Daniel Perna <danielperna84@gmail.com>
2019-12-16 01:25:08 +0100

    Add XML-locations

--- pydevccu/hmip_xml_to_json.py
+++ pydevccu/hmip_xml_to_json.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 Convert XML-files for HomeMaticIP and HomeMaticIP Wired to JSON files.
+Extract /opt/HMServer/HMIPServer.jar (zip).
+Relevant folders:
+- de/eq3/cbcs/devicedescription/channelspecification/ (Channel definitions)
+- de/eq3/cbcs/devicedescription/devicespecification/eQ-3/ (Device descriptions)
 """
