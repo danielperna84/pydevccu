ac91d09d4401639ab56d3dafe4827cd7207622db
Daniel Perna <danielperna84@gmail.com>
2019-12-16 01:25:37 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 PACKAGE_NAME = 'pydevccu'
 HERE = os.path.abspath(os.path.dirname(__file__))
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 
 PACKAGES = find_packages(exclude=['tests', 'tests.*', 'dist', 'build'])
 
